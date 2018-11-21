6a1043eee9e77b5ef2ddefa804f31e1c77d181b5
Kai Reinhard <K.Reinhard@micromata.de>
2018-11-21 07:58:20 +0100

    More tests.

--- paypal-core/src/test/java/de/micromata/paypal/PaymentTest.java
+++ paypal-core/src/test/java/de/micromata/paypal/PaymentTest.java
@@ -1,30 +1,42 @@
 package de.micromata.paypal;
 
-import de.micromata.paypal.data.Currency;
-import de.micromata.paypal.data.Details;
-import de.micromata.paypal.data.Payment;
-import de.micromata.paypal.data.Transaction;
+import de.micromata.paypal.data.*;
 import de.micromata.paypal.json.JsonUtils;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PaymentTest {
+    private static Logger log = LoggerFactory.getLogger(PaymentTest.class);
 
     @Test
     void paymentTest() {
         PayPalConfig config = PayPalConfig.createDemoConfig();
-        Payment payment = new Payment();
         Transaction transaction = new Transaction(Currency.EUR);
         transaction.setInoviceNumber(generateString(200));
         assertEquals(127, transaction.getInoviceNumber().length());
         assertEquals(generateString(124) + "...", transaction.getInoviceNumber());
         transaction.addItem("Online Elections 2019", 29.99).setTax(5.70);
-        payment.addTransaction(transaction).setNoteToPayer("Enjoy your Elections with POLYAS.");
+        Payment payment = new Payment(transaction).setNoteToPayer("Enjoy your Elections with POLYAS.");
         payment.setConfig(config);
         payment.recalculate();
         assertEquals("35.69", transaction.getAmount().getTotal().toString());
-        System.out.println(JsonUtils.toJson(payment, true));
+        assertEquals("29.99", transaction.getAmount().getDetails().getSubtotal().toString());
+
+        transaction.addItem("Logo", 1.00).setTax(.19);
+        payment.recalculate();
+        assertEquals("36.88", transaction.getAmount().getTotal().toString());
+        assertEquals("30.99", transaction.getAmount().getDetails().getSubtotal().toString());
+        assertEquals("5.89", transaction.getAmount().getDetails().getTax().toString());
+
+        transaction.getAmount().getDetails().setShipping(3.99);
+        payment.recalculate();
+        assertEquals("40.87", transaction.getAmount().getTotal().toString());
+        assertEquals("30.99", transaction.getAmount().getDetails().getSubtotal().toString());
+        assertEquals("5.89", transaction.getAmount().getDetails().getTax().toString());
+        log.info(JsonUtils.toJson(payment, true));
     }
 
     private String generateString(int length) {
